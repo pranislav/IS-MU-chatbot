5357c8bd50f7fb626a34603a3d9d3c7df6ebf924
Branislav Paluch <484757@mail.muni.cz>
2025-03-30 16:05:47 +0200

    add switch to test chat template

--- src/chat.py
+++ src/chat.py
@@ -8,8 +8,18 @@ warnings.filterwarnings("ignore", message="Found missing adapter keys while load
 def chat(model, tokenizer, question):
     messages = [{"role": "user", "content": question}]
     formatted_text = tokenizer.apply_chat_template(messages, tokenize=False)
+    print(formatted_text)
 
     inputs = tokenizer(formatted_text, return_tensors="pt").to("cuda")
+    output = model.generate(**inputs, max_new_tokens=100)
+    return tokenizer.decode(output[0], skip_special_tokens=True)
+
+
+def chat_simple(model, tokenizer, question):
+    '''Demonstrates the difference when not using the chat template
+    Switch to this function in the main to see the difference
+    '''
+    inputs = tokenizer(question, return_tensors="pt").to("cuda")
     output = model.generate(**inputs, max_new_tokens=100)
     return tokenizer.decode(output[0], skip_special_tokens=True)
 
@@ -29,7 +39,8 @@ def load_model(adapter_path=None):
     return model, tokenizer
 
 def main():
-    parser = argparse.ArgumentParser(description="Chat with the model. Provide path to LoRA adapter or leave empty for original model.")
+    parser = argparse.ArgumentParser(description="Chat with the model. Just last question is in the context window. Provide path to LoRA adapter or leave empty for original model. Toggle between input formatted with chat template and simple input by typing 'switch' in the input.")
+    parser.add_argument("--model_name", type=str, default="google/gemma-3-4b-it", help="Name of the model to load.")
     parser.add_argument("--adapter_path", type=str, default=None, help="Path to the LoRA adapter.")
     args = parser.parse_args()
 
@@ -37,11 +48,22 @@ def main():
 
     print("Start chatting with the model (type 'quit' to exit)...")
 
+    chat_func = chat
+
     while True:
         question = input()
         if question.lower() == 'quit':
             break
-        answer = chat(model, tokenizer, question)
+        if question.lower() == 'switch':
+            if chat_func == chat:
+                chat_func = chat_simple
+                print("Switched to simple chat function.")
+                continue
+            else:
+                chat_func = chat
+                print("Switched to chat function with template.")
+                continue
+        answer = chat_func(model, tokenizer, question)
         print(answer)
 
 
